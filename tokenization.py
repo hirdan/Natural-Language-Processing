3cd9d575798121fa4de88f217825b65782fc65bd
Hirdan Aggarwal <aggarwalhirdan@gmail.com>
2020-03-26 11:36:27 +0530

    Add files via upload

---
+++ tokenization.py
@@ -0,0 +1,24 @@
+##import nltk
+##
+##nltk.download()
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+##tokenizing(form of grouping things) - word tokenizers....sentence tokenizers
+##lexicon and corporas
+##corpora - body of text. ex: medical journals, presidential speeches, English language
+##lexicon - wods and their means
+
+##investor-speak....regular english-speak
+
+##investor-speak 'bull' - someone who is positive about the market
+##english-speak 'bull' - scary animal you dont want running at you
+
+
+example_text = "Hello Mr. Smith, how are you doind today? The weather is great and Python is awesome. The sky is pinkish-blue. You should not eat cardboard."
+
+##print(sent_tokenize(example_text))
+##print(word_tokenize(example_text))
+
+for i in word_tokenize(example_text):
+    print(i)
